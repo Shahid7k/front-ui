65c913dc21ef307839b7e5d93b4a849d43de2152
shraddha <shraddhapatalay11@gmail.com>
2020-05-26 00:29:35 +0530

    minor fixes

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -29,55 +29,14 @@ const Navbar = ({ title, icon }) => {
         </NavLink>
       </h1>
 
-      {userAuth.token ? getAuthLinks(handleLogout) : getNoAuthLinks()}
+      {userAuth.token
+        ? getAuthLinks(userAuth.user.firstName, handleLogout)
+        : getNoAuthLinks()}
     </div>
   );
 };
 
-// const getNavItems = () => {
-//   return (
-//     <>
-//       <NavLink to={DASHBOARD} className='listItem text-decoration-none'>
-//         {'Dashboard'}
-//       </NavLink>
-//       <NavLink to='#' className='listItem text-decoration-none'>
-//         {'Explore'}
-//       </NavLink>
-//       <NavLink to='#' className='listItem text-decoration-none'>
-//         {'title'}
-//       </NavLink>
-//     </>
-//   );
-// };
-
 const getNoAuthLinks = () => (
-  // <div className='dropdown'>
-  //   <button
-  //     className='btn btn-dark nobg dropdown-toggle font14'
-  //     type='button'
-  //     id='dropdownMenuButton'
-  //     data-toggle='dropdown'
-  //     aria-haspopup='true'
-  //     aria-expanded='false'
-  //   >
-  //     {'Log In'}
-  //   </button>
-  //   <div className='dropdown-menu font12' aria-labelledby='dropdownMenuButton'>
-  //     <NavLink
-  //       to={SIGNIN_ROUTE}
-  //       className='text-dark dropdown-item text-decoration-none'
-  //     >
-  //       Sign In
-  //     </NavLink>
-  //     <NavLink
-  //       to={SIGNUP_ROUTE}
-  //       className='text-dark dropdown-item text-decoration-none'
-  //     >
-  //       Sign Up
-  //     </NavLink>
-  //   </div>
-  // </div>
-
   <ul className='my-auto'>
     <li>
       <NavLink to={SIGNIN_ROUTE} className='text-decoration-none'>
@@ -92,31 +51,7 @@ const getNoAuthLinks = () => (
   </ul>
 );
 
-const getAuthLinks = handleLogout => (
-  // <ul className='my-auto'>
-  //   <li>
-  //     <NavLink to={HOME_ROUTE} className='text-decoration-none'>
-  //       Home
-  //     </NavLink>
-  //   </li>
-  //   <li>
-  //     <NavLink to={DASHBOARD} className='text-decoration-none'>
-  //       Dashboard
-  //     </NavLink>
-  //   </li>
-  //   <li>
-  //     <NavLink to={PROFILE} className='text-decoration-none'>
-  //       Profile
-  //     </NavLink>
-  //   </li>
-
-  //   <li>
-  //     <NavLink to='#' className='text-decoration-none' onClick={handleLogout}>
-  //       Log out
-  //     </NavLink>
-  //   </li>
-  // </ul>
-
+const getAuthLinks = (firstName, handleLogout) => (
   <Fragment>
     <ul className='my-auto ml-auto'>
       <li>
@@ -132,6 +67,7 @@ const getAuthLinks = handleLogout => (
     </ul>
     <div className='dropdown'>
       <button
+        style={{ minWidth: '120px' }}
         className='btn btn-dark nobg dropdown-toggle'
         type='button'
         id='dropdownMenuButton'
@@ -139,7 +75,7 @@ const getAuthLinks = handleLogout => (
         aria-haspopup='true'
         aria-expanded='false'
       >
-        {'Welcome User'}
+        {firstName || ''}
       </button>
       <div
         className='dropdown-menu font12'
